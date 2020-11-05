0729d1270cd5fb8fe2983467c506e5018c0bf0cc
Thomas Klytta <thomas.klytta@gmail.com>
2020-11-05 09:07:36 +0100

    Refactoring enemies to set different health

--- Game/DemonEnemy.cs
+++ Game/DemonEnemy.cs
@@ -4,7 +4,8 @@
     {
         public DemonEnemy(int x, int y) : base(x, y)
         {
-
+            // Jeder Demon hat 80 Health
+            Health = 80;
         }
 
         public override char GetSymbol()
--- Game/OrcEnemy.cs
+++ Game/OrcEnemy.cs
@@ -1,10 +1,14 @@
-﻿namespace Game
+﻿using System;
+
+namespace Game
 {
     public class OrcEnemy : Enemy
     {
         public OrcEnemy(int x, int y) : base(x, y)
         {
-
+            // Random health von 100 bis 200 bei Generierung von einem Orc
+            Random random = new Random();
+            Health = random.Next(100, 200);
         }
 
         public override char GetSymbol()
